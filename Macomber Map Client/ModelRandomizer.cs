12309ed5a252df077d4b0d9371b4414aa568c43e
MichaelLegatt_cp <ResilientGrid@users.noreply.github.com>
2013-12-31 04:23:18 +0000

    Updated randomization code, and added the randomized model to the project.

--- Macomber Map Client/ModelRandomizer.cs
+++ Macomber Map Client/ModelRandomizer.cs
@@ -30,10 +30,10 @@ namespace Macomber_Map
         private static RNGCryptoServiceProvider csp = new System.Security.Cryptography.RNGCryptoServiceProvider();
 
         /// <summary>Our collection of assigned TEIDs to prevent duplication</summary>
-        private static Dictionary<int, bool> AssignedTEIDs = new Dictionary<int,bool>();
+        private static Dictionary<int, bool> AssignedTEIDs = new Dictionary<int, bool>();
 
         /// <summary>Our collection of dictionary words</summary>
-        private static List<String> DictionaryWords = new List<string>();        
+        private static List<String> DictionaryWords = new List<string>();
         #endregion
 
 
@@ -49,67 +49,133 @@ namespace Macomber_Map
 
             //Load our dictionary in memory.
             //For the open source version demo, I used http://wordlist.sourceforge.net/ english-proper-names.95 file.
+            //Insert your own list of forbidden words below to strip out any words that should be excluded from the random name selection process
+            DictionaryWords.Clear();
             String InLine;
-           using (StreamReader sRd = new StreamReader(DictionaryList))
-            while ((InLine = sRd.ReadLine()) != null)
-                if (!InLine.Trim().EndsWith("'s"))
-                    DictionaryWords.Add(InLine.Trim());
-
+            String[] ForbiddenWords = "".ToLower().Split(',');
+            using (StreamReader sRd = new StreamReader(DictionaryList))
+                while ((InLine = sRd.ReadLine()) != null)
+                {
+                    bool WordOk = !InLine.Trim().EndsWith("'s");
+                    foreach (String ForbiddenWord in ForbiddenWords)
+                        if (InLine.ToLower().Contains(ForbiddenWord))
+                            WordOk = false;
+                    if (WordOk)
+                        DictionaryWords.Add(InLine.Trim());
+                }
 
             //Load our XML file into memory
             XmlDocument xDoc = new XmlDocument();
             xDoc.Load(XmlFileName);
 
-           
+
             //Go through all TEID attributes and create our updated list
             String BasePath = Path.Combine(Path.GetDirectoryName(XmlFileName), Path.GetFileNameWithoutExtension(XmlFileName));
+            UpdatedTEIDsAndContingencies.Clear();
+            OriginalTEIDsAndContingencies.Clear();
+            AssignedTEIDs.Clear();
             UpdatedTEIDsAndContingencies.Add("0", "0");
-            OriginalTEIDsAndContingencies.Add("0", "0");
-            foreach (XmlAttribute xAttr in xDoc.SelectNodes("//@TEID"))
-                    if (xAttr.Value != "0" && !UpdatedTEIDsAndContingencies.ContainsKey(xAttr.Value))
-                    {
-                        String OldTEID = xAttr.Value;
-                        String NewTEID = GenerateTEID();
-                        UpdatedTEIDsAndContingencies.Add(OldTEID, NewTEID);
-                        OriginalTEIDsAndContingencies.Add(NewTEID,OldTEID);
-                    }
+            String FoundValue;
+            XmlNodeList NodesToParse;
 
-                foreach (XmlElement xElem in xDoc.SelectNodes("//Contingency"))
-                {
-                    String OldName = xElem.Attributes["Name"].Value;
-                    RandomizeString(xElem.Attributes["Name"], 1, true);
-                    UpdatedTEIDsAndContingencies.Add(OldName, xElem.Attributes["Name"].Value);
-                    OriginalTEIDsAndContingencies.Add(xElem.Attributes["Name"].Value, OldName);
-                }
-            
-
-
-            //Go through every element and update attributes as appropriate
-            String[] TEIDsToUpdate = "PhysicalUnit,LogicalUnits,Elements,RASs,Substation,Substation1,Substation2,Substations,ConnectedStations,BusbarSection,Contingencies,Node,TEID,Loads,BusbarSections,Transformers,Units,Onwer,Owners,Operator,Operators,StaticVarCompensators,ShuntCompensators,ViolatedElement,Winding1,Winding2,Windings".Split(',');
-            String[] TitlesToUpdate = "Name,LongName,Description".Split(',');
-            List<String> SeenElements = new List<string>();
-
-
-            //Go through each of our XML elements and update accordingly
-            int Percentage= xDoc.DocumentElement.ChildNodes.Count/100;
-            for (int CurNode=0; CurNode < xDoc.DocumentElement.ChildNodes.Count; CurNode++)
+            String[] TEIDsToUpdate = "TEID,LineTEID,PhysicalUnit,LogicalUnits,Unit,Load,Elements,RASs,Substation,Substation1,Substation2,Substations,ConnectedStations,BusbarSection,Node,Loads,BusbarSections,Transformers,Transformer,Units,Owner,Operator,StaticVarCompensators,ShuntCompensators,ViolatedElement,Winding1,Winding2,Windings,AssociatedLine".Split(',');
+            Dictionary<String, int> UpdateCount = new Dictionary<string, int>();
+            foreach (String AttributeToUpdate in TEIDsToUpdate)
             {
-                XmlNode xNode = xDoc.DocumentElement.ChildNodes[CurNode];
-                if (CurNode % Percentage == 0)
+                NodesToParse = xDoc.SelectNodes("//@" + AttributeToUpdate);
+                Console.WriteLine("Updating TEIDs in " + AttributeToUpdate+": " + NodesToParse.Count.ToString("#,##0"));
+                UpdateCount.Add(AttributeToUpdate, NodesToParse.Count);
+                foreach (XmlAttribute xAttr in NodesToParse)
                 {
-                    Console.WriteLine("Parsing nodes: " + (CurNode / Percentage).ToString() + "%");
-                    Application.DoEvents();
+                    String[] TEIDs = xAttr.Value.Split(',');
+                    for (int a = 0; a < TEIDs.Length; a++)
+                    {
+                        if (!UpdatedTEIDsAndContingencies.TryGetValue(TEIDs[a], out FoundValue))
+                        {
+                            UpdatedTEIDsAndContingencies.Add(TEIDs[a], FoundValue = GenerateTEID());
+                            OriginalTEIDsAndContingencies.Add(FoundValue, TEIDs[a]);
+                        }
+                        TEIDs[a] = FoundValue;
+                    }
+                    xAttr.Value = String.Join(",", TEIDs);
                 }
-                if (!SeenElements.Contains(xNode.Name))
-                {
-                    Console.WriteLine("Encountered new node type: " + xNode.Name);
-                    SeenElements.Add(xNode.Name);
-                }
-
-                UpdateNode(xNode, TEIDsToUpdate, TitlesToUpdate);
-                foreach (XmlNode xChild in xNode.ChildNodes)
-                    UpdateNode(xChild, TEIDsToUpdate, TitlesToUpdate);                              
             }
+
+            
+            //Now, go through our names and update accordingly.
+            String[] TitlesToUpdate = "Alias,Name,Contingencies,LongName,Description,Target".Split(',');
+            foreach (String AttributeToUpdate in TitlesToUpdate)
+            {
+                NodesToParse = xDoc.SelectNodes("//@" + AttributeToUpdate);
+                UpdateCount.Add(AttributeToUpdate, NodesToParse.Count);
+                Console.WriteLine("Updating Names in " + AttributeToUpdate + ": " + NodesToParse.Count.ToString("#,##0"));
+                foreach (XmlAttribute xAttr in NodesToParse)
+                    if (!xAttr.OwnerElement.Name.StartsWith("MM_"))
+                    {
+                        String[] Names = xAttr.Value.Split(',');
+                        for (int a = 0; a < Names.Length; a++)
+                        {
+                            if (xAttr.OwnerElement.Name == "County" || xAttr.OwnerElement.Name == "State")
+                            { }
+                            else if ((xAttr.OwnerElement.Name == "Contingency" && xAttr.Name == "Name") || (xAttr.OwnerElement.Name == "Company" && xAttr.Name == "Alias"))
+                            {
+                                if (!UpdatedTEIDsAndContingencies.TryGetValue(Names[a], out FoundValue))
+                                    UpdatedTEIDsAndContingencies.Add(Names[a], FoundValue = RandomizeSubString(Names[a], 1));
+                                Names[a] = FoundValue;
+
+                            }
+                            else
+                            {
+                                if (!UpdatedTEIDsAndContingencies.TryGetValue(Names[a], out FoundValue))
+                                    UpdatedTEIDsAndContingencies.Add(Names[a], FoundValue = GenerateRandomWord(Names[a]));
+                                Names[a] = FoundValue;
+                            }
+                        }
+                        xAttr.Value = String.Join(",", Names);
+                    }
+            }
+
+
+            //Update our companies
+            NodesToParse = xDoc.SelectNodes("/NetworkModel/Company");
+            UpdateCount.Add("<Company>", NodesToParse.Count);
+
+            Console.WriteLine("Updating company phone number and DUNs: " + NodesToParse.Count.ToString("#,##0"));
+            foreach (XmlElement xNode in NodesToParse)
+            {
+                RandomizeNumber(xNode.Attributes["PrimaryPhone"]);
+                RandomizeNumber(xNode.Attributes["DUNS"]);
+            }
+
+
+            //Update our substation coordinates
+            Dictionary<String, String> SubstationCoordinates = new Dictionary<string, string>();
+            NodesToParse = xDoc.SelectNodes("/NetworkModel/Substation");
+            UpdateCount.Add("<Substation>", NodesToParse.Count);
+            Console.WriteLine("Updating substation coordinates: " + NodesToParse.Count.ToString("#,##0"));
+            foreach (XmlElement xNode in NodesToParse)
+            {
+                RandomizePosition(xNode.Attributes["Longitude"]);
+                RandomizePosition(xNode.Attributes["Latitude"]);
+                xNode.Attributes["LatLong"].Value = xNode.Attributes["Longitude"].Value + "," + xNode.Attributes["Latitude"].Value;
+                SubstationCoordinates.Add(xNode.Attributes["TEID"].Value, xNode.Attributes["LatLong"].Value);
+            }
+
+
+            //Update our lines and DC ties
+            foreach (String LineAttribute in "Line,DCTie".Split(','))
+            {
+                NodesToParse = xDoc.SelectNodes("/NetworkModel/" + LineAttribute);
+                UpdateCount.Add("<" + LineAttribute + ">", NodesToParse.Count);
+                Console.WriteLine("Updating substation coordinates for " + LineAttribute + ": " + NodesToParse.Count.ToString("#,##0"));
+                foreach (XmlNode xNode in NodesToParse)
+                {
+                    String LatLong1 = SubstationCoordinates[xNode.Attributes["Substation1"].Value];
+                    String LatLong2 = SubstationCoordinates[xNode.Attributes["Substation2"].Value];
+                    xNode.Attributes["Coordinates"].Value = LatLong1 + "," + LatLong2;
+                }
+            }
+
 
             //Now, save our updated XML file and translation dictionary
             using (StreamWriter sW = new StreamWriter(BasePath + "-TEIDAndContingencyDeltas.csv", false, new UTF8Encoding(false)))
@@ -119,48 +185,14 @@ namespace Macomber_Map
                     sW.WriteLine("\"{0}\",\"{1}\"", kvp.Key, kvp.Value);
             }
 
-            using (XmlTextWriter xW = new XmlTextWriter(BasePath + "-Updated.MM_Model",new UTF8Encoding(false)))
+            using (XmlTextWriter xW = new XmlTextWriter(BasePath + "-Updated.MM_Model", new UTF8Encoding(false)))
             {
                 xW.Formatting = Formatting.Indented;
                 xDoc.WriteTo(xW);
             }
         }
 
-        /// <summary>
-        /// Update a node
-        /// </summary>
-        /// <param name="xNode"></param>
-        /// <param name="TEIDsToUpdate"></param>
-        /// <param name="TitlesToUpdate"></param>
-        private static void UpdateNode(XmlNode xNode, String[] TEIDsToUpdate, String[] TitlesToUpdate)
-        {
-            foreach (String str in TEIDsToUpdate)
-                UpdateTEIDs(xNode.Attributes[str]);
-
-            foreach (String str in TitlesToUpdate)
-                if (str != "Name" || (xNode.Name != "Contingency" && xNode.Name != "County" && xNode.Name != "State"))
-                    RandomizeString(xNode.Attributes[str]);
-
-            if (xNode.Name == "Company")
-            {
-                RandomizeNumber(xNode.Attributes["PrimaryPhone"]);
-                RandomizeNumber(xNode.Attributes["DUNS"]);
-                RandomizeString(xNode.Attributes["Alias"], 1, true);
-            }
-            else if (xNode.Name == "Substation")
-            {
-                RandomizePosition(xNode.Attributes["Longitude"]);
-                RandomizePosition(xNode.Attributes["Latitude"]);
-                xNode.Attributes["LatLong"].Value = xNode.Attributes["Longitude"].Value + "," + xNode.Attributes["Latitude"].Value;
-            }
-            else if (xNode.Name == "Line" || xNode.Name == "DCTie")
-            {
-                xNode.Attributes["LineTEID"].Value = GenerateTEID();
-                XmlElement xSub1 = xNode.OwnerDocument.SelectSingleNode("//Substation[@TEID='" + xNode.Attributes["Substation1"].Value + "']") as XmlElement;
-                XmlElement xSub2 = xNode.OwnerDocument.SelectSingleNode("//Substation[@TEID='" + xNode.Attributes["Substation1"].Value + "']") as XmlElement;
-                xNode.Attributes["Coordinates"].Value = xSub1.Attributes["LatLong"].Value + "," + xSub2.Attributes["LatLong"].Value;
-            }
-        }
+    
 
         /// <summary>
         /// Generate a new TEID
@@ -189,31 +221,7 @@ namespace Macomber_Map
             xAttr.Value = CurVal.ToString();
         }
 
-        /// <summary>
-        /// Update our list of TEIDs
-        /// </summary>
-        /// <param name="xAttr"></param>
-        private static void UpdateTEIDs(XmlAttribute xAttr)
-        {
-            if (xAttr == null)
-                return;
-            String[] InVal = xAttr.Value.Split(',');
-            string FoundVal;
-            for (int a = 0; a < InVal.Length; a++)
-                if (UpdatedTEIDsAndContingencies.TryGetValue(InVal[a], out FoundVal))
-                    InVal[a] = FoundVal;
-                else
-                {
-                    Console.WriteLine(xAttr.OwnerElement.Name + ": Can't find " + InVal[a] + " for " + xAttr.Name);
-                    Application.DoEvents();
-                    FoundVal = RandomizeSubString(InVal[a], 1);
-                    UpdatedTEIDsAndContingencies.Add(InVal[a], FoundVal);
-                    OriginalTEIDsAndContingencies.Add(FoundVal, InVal[a]);
-                    InVal[a] = FoundVal;
-                }   
-            xAttr.Value = String.Join(",", InVal);
-        }
-
+       
         /// <summary>
         /// Randomize a number
         /// </summary>
@@ -245,35 +253,28 @@ namespace Macomber_Map
                     else if (Char.IsLower(CurVal[a]))
                         CurVal[a] = (char)((int)'a' + RandomInteger(0, 26));
             } while (OriginalTEIDsAndContingencies.ContainsKey(new string(CurVal)));
+            OriginalTEIDsAndContingencies.Add(new string(CurVal), InString);
             return new string(CurVal);
         }
+
         /// <summary>
-        /// Generate a random string based on our text
+        /// Generate a random word from our dictionary
         /// </summary>
-        /// <param name="xAttr"></param>
-        /// <param name="StartPos"></param>
-        /// <param name="PreserveLength"></param>
-        private static void RandomizeString(XmlAttribute xAttr, int StartPos = 0, bool PreserveLength = false)
+        /// <param name="InString"></param>
+        /// <returns></returns>
+        private static String GenerateRandomWord(String InString)
         {
-            if (xAttr == null)
-                return;
-            else if (PreserveLength)
-                xAttr.Value = RandomizeSubString(xAttr.Value, StartPos);
-            else if (xAttr.Name != "Name")
-                xAttr.Value= MM_Repository.TitleCase(DictionaryWords[RandomInteger(0, DictionaryWords.Count)]);
-            else
+            String CurVal = InString;
+            do
             {
-                String CurVal = xAttr.Value;
-                do
-                {
-                    int CurWord = RandomInteger(0, DictionaryWords.Count);
-                    CurVal = MM_Repository.TitleCase(DictionaryWords[CurWord]);
-                    DictionaryWords.RemoveAt(CurWord);
-                } while (OriginalTEIDsAndContingencies.ContainsKey(CurVal));
-                OriginalTEIDsAndContingencies.Add(CurVal, xAttr.Value);
-                xAttr.Value = CurVal;
-            }
+                int CurWord = RandomInteger(0, DictionaryWords.Count);
+                CurVal = MM_Repository.TitleCase(DictionaryWords[CurWord]);
+                DictionaryWords.RemoveAt(CurWord);
+            } while (OriginalTEIDsAndContingencies.ContainsKey(CurVal));
+            OriginalTEIDsAndContingencies.Add(CurVal, InString);
+            return CurVal;
         }
+      
 
         /// <summary>
         /// Use our random number generator to create an integer between an upper bound (exclusive) and lower bound (inclusive)
@@ -305,9 +306,6 @@ namespace Macomber_Map
         /// Use our random number generator to create a double-precision number
         /// Thanks to http://msdn.microsoft.com/en-us/magazine/cc163367.aspx
         /// </summary>
-        /// <param name="rng"></param>
-        /// <param name="Minimum"></param>
-        /// <param name="Maximum"></param>
         /// <returns></returns>
         private static double RandomDouble()
         {
@@ -316,8 +314,5 @@ namespace Macomber_Map
             UInt32 rand = BitConverter.ToUInt32(RndBuffer, 0);
             return rand / (1.0 + UInt32.MaxValue);
         }
-
-         
-        
     }
 }
